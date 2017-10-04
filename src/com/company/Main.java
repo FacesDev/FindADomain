477e31f24d080e45ec9324ab8f35ce3b55ee642b
Chris <kitecabrinha@gmail.com>
2017-10-04 16:55:14 -0400

    added time stamps to start and finish

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,17 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
-
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
             public void parseFile(String fileName, String searchStr) throws FileNotFoundException {
-                System.out.println("here");
+
+                LocalDateTime now = LocalDateTime.now();
+                System.out.println("Start Time:" + dtf.format(now));
                 Scanner scan = new Scanner(new File(fileName));
                 while (scan.hasNext()) {
                     String line = scan.nextLine().toLowerCase().toString();
@@ -16,11 +20,12 @@ public class Main {
                         System.out.println(line);
                     }
                 }
+                System.out.println("Finish Time:" + dtf.format(now));
             }
             public static void main(String[] args) throws FileNotFoundException {
-                System.out.println("here");
                 Main fileSearch = new Main();
                 fileSearch.parseFile("/Users/solderedmachd/Desktop/domainSearch/src/com/company/com.", "soteria");
             }
-
 }
+
+
